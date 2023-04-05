5032f5278fc28b6ac3a510b38e2a5371238485af
soyUnaGata <fallen.snitch@gmail.com>
2023-04-05 17:21:57 +0300

    add mobile style for 480px

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import '@/assets/css/main.css'
+import '@/assets/css/mobile.css'
 import Vue3VideoPlayer from '@cloudgeek/vue3-video-player'
 import '@cloudgeek/vue3-video-player/dist/vue3-video-player.css'
 
